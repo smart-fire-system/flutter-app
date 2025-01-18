8847e22c3ca948847d7564be89c82469abaccb3e
Ahmed Hassan <ahmadmhasann@gmail.com>
2025-01-18 15:02:57 +0200

    rename files

--- lib/screens/branches/view/view.dart
+++ lib/screens/branches/view/view.dart
@@ -1,8 +1,8 @@
 export 'branches_screen.dart';
-export 'branches_details.dart';
-export 'branches_add.dart';
-export 'branches_edit.dart';
+export 'branch_details.dart';
+export 'branch_add.dart';
+export 'branch_edit.dart';
 export 'companies_screen.dart';
-export 'companies_details.dart';
-export 'companies_add.dart';
-export 'companies_edit.dart';
+export 'company_details.dart';
+export 'company_add.dart';
+export 'company_edit.dart';
